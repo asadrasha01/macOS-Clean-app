c184e9a320e618333b3bd3e5c0501a7f49f5dc90
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2023-10-13 13:59:14 +0300

    SimpleGUI

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import PySimpleGUI as sg
 import quit_app
 import clean_bin
 
-options = ['Quit all app', 'Clean bin', 'Clear cache']
-i = 0
-for option in options:
-    i+=1
-    print(f'{i})',option)
-user_input = int(input("User choice: "))
+sg.theme('Dark Grey 13')   
 
-if user_input == 1:
-    quit_app.quit_all_app()
-elif user_input == 2:
-    clean_bin.cleaning_bin()
+layout = [  [sg.Text('Welcome to MacOS Cleaner')],
+            [sg.Text('Choose:'), ],
+            [sg.Button('Quit all app'), sg.Button('Clean bin')] ]
+
+window = sg.Window("MacOS Clean", layout)
+
+while True:
+    event, values = window.read()
+    if event == 'Quit all app':
+        try:
+            quit_app.quit_all_app()
+            sg.popup('All applications successfully have been closed')
+            break
+        except Exception as e:
+            sg.popup(f"An error occurred: {e}")
+    elif event == 'Clean bin':
+        try:
+            clean_bin.cleaning_bin()
+            sg.popup('Your bin has been cleaned')
+            break
+        except Exception as e:
+            sg.popup(f"An error occurred: {e}")
+    elif event == sg.WINDOW_CLOSED:
+        break
+
+window.close()
--- quit_app.py
+++ quit_app.py
@@ -2,12 +2,13 @@ import subprocess
 
 def quit_all_app():
     script = '''
+    with timeout of 600 seconds
         tell application "System Events"
             set quitapps to name of every application process whose background only is false and name is not "Finder" and name is not "Terminal"
         end tell
         repeat with closeapp in quitapps
             tell application closeapp to quit
         end repeat
+    end timeout
     '''
     subprocess.run(['osascript', '-e', script])
-print("Applications which you chose has been succesfully quit")
